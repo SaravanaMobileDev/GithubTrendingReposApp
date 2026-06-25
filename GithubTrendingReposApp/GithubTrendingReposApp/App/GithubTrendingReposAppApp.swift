f812f53467b2567196a39f68aca085d8c355676d
SaravanaMobileDev <sharvan4701@gmail.com>
2026-06-26 00:20:29 +0530

    feat - folder structure and initial commit for the project requirements

---
+++ GithubTrendingReposApp/GithubTrendingReposApp/App/GithubTrendingReposAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTrendingReposAppApp.swift
+//  GithubTrendingReposApp
+//
+//  Created by Saravana Ganesh L on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GithubTrendingReposAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
